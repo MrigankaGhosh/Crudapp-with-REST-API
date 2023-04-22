082923b6c42f1567a66091288d1486e193b93ee3
Mriganka <mrigankaghosh90@gmail.com>
2023-04-23 00:13:35 +0530

    openapi documentation added

--- src/main/java/com/restfuldatajpa/personal/crudapp/CrudappApplication.java
+++ src/main/java/com/restfuldatajpa/personal/crudapp/CrudappApplication.java
@@ -3,7 +3,11 @@ package com.restfuldatajpa.personal.crudapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Crudapp", version = "1.0", description = "built a rest api"))
 public class CrudappApplication {
 
 	public static void main(String[] args) {
